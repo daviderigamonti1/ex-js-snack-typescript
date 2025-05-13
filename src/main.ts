7a578d6081b07b018d3a07bc236e54d51c434683
daviderigamonti1 <davide.rigamonti.01@gmail.com>
2025-05-13 19:23:22 +0200

    Snack 2

--- src/main.ts
+++ src/main.ts
@@ -36,11 +36,23 @@ if (typeof data === "string") {
 // annoNascita → numero
 // sesso → Può essere solo "m" o "f".
 // anniDiServizio (array di numeri, es. [2014, 2015, 2017, 2018])
+
 // 🎯 BONUS
 // Il type alias Dipendente, ha anche i seguenti dati:
 
 // emailAziendale → Email assegnata al dipendente (non si può modificare)
 // contratto → Specifica il tipo di contratto del dipendente, con valori limitati a “indeterminato”, “determinato” o “freelance”.
+
+type Dipendente = {
+  nome: string,
+  cognome: string,
+  annoNascita: number,
+  sesso: "m" | "f",
+  anniDiServizio: number[],
+  readonly emailAziendale: string,
+  contratto: "indeterminato" | "determinato" | "freelance"
+}
+
 // 🏆 Snack 3
 // Estendiamo Dipendente per definire due ruoli specifici all'interno dell'azienda:
 
